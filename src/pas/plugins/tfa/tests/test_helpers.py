8e38ee4a7179f0d4e3b8485e828e4d07bb5629df
1letter <1letter@gmx.de>
2024-05-05 14:48:13 +0200

    Fix test

--- src/pas/plugins/tfa/tests/test_helpers.py
+++ src/pas/plugins/tfa/tests/test_helpers.py
@@ -16,10 +16,7 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer["portal"]
-        if get_installer:
-            self.installer = get_installer(self.portal, self.layer["request"])
-        else:
-            self.installer = api.portal.get_tool("portal_quickinstaller")
+        self.installer = get_installer(self.portal, self.layer["request"])
 
     def test_sign_url(self):
         login = "user1"
