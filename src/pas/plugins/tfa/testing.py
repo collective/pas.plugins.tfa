96cd0288214dd9f569d43b1eaa18c389287ff412
1letter <1letter@gmx.de>
2024-05-05 23:09:04 +0200

    fix FIXTURE

--- src/pas/plugins/tfa/testing.py
+++ src/pas/plugins/tfa/testing.py
@@ -1,16 +1,16 @@
-from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.testing import z2
+from plone.testing.zope import WSGI_SERVER_FIXTURE
 
 import pas.plugins.tfa
 
 
 class PasPluginsOtpLayer(PloneSandboxLayer):
-    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+    defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         # Load any other ZCML that is required for your tests.
@@ -42,7 +42,7 @@ PAS_PLUGINS_OTP_ACCEPTANCE_TESTING = FunctionalTesting(
     bases=(
         PAS_PLUGINS_OTP_FIXTURE,
         REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
+        WSGI_SERVER_FIXTURE,
     ),
     name="PasPluginsOtpLayer:AcceptanceTesting",
 )
