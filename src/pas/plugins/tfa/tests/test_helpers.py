3c8ae358bdbc3fcec9ee4ee44ed5142cea2c117f
1letter <1letter@gmx.de>
2024-05-05 23:12:29 +0200

    Update tests

--- src/pas/plugins/tfa/tests/test_helpers.py
+++ src/pas/plugins/tfa/tests/test_helpers.py
@@ -1,11 +1,19 @@
 """Setup tests for this package."""
 
 from pas.plugins.tfa import helpers
+from pas.plugins.tfa.testing import PAS_PLUGINS_OTP_FUNCTIONAL_TESTING  # noqa: E501
 from pas.plugins.tfa.testing import PAS_PLUGINS_OTP_INTEGRATION_TESTING  # noqa: E501
-from plone import api
-from plone.base.utils import get_installer
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.testing.zope import Browser
 
+import transaction
 import unittest
+
+
+QR_CODE_REF = "iVBORw0KGgoAAAANSUhEUgAAAZoAAAGaAQAAAAAefbjOAAADGElEQVR4nO2bS46bQBCGvwpIXjY3mKPAzUY5Um4AR/EBIsHSEujPorqBmckmcYI9Q7FAfn1ytyjV469qE398Dd/+nIGAAgoooIACCuhrQpavGusms/zdVJdfTeUH3UOWF9DxUCtJGkE9ldw2rIFiFpUkSW+h45YX0PHQlB2AdSzmtqFxMbOXm7sMM6vfQ8ctL6CHQfreZLOwjkpAJev+xz8F9EmgVjMMDTA0i30E/t0/BfTUUJLUA2Z2ESSJdqxEO4J6QNL8uOUFdDQ0mJmnku0ItNetzKixjsVLjUctL6DjIH/ym5QtyKmkYPYbpLda95PvKaB7ILyqbMdqV316hGg1u8uQNCONlbxK7Z98TwHdD+l7s5jZiwTpZtZNNTBdBGnOucXwEnnEGaB6/2aqZ7eDXGtgGgw0dIsBdTGJJ99TQPdAHjVy6JCUo8a43QCovP7wK6LGV4ZyHkGagWIMuAkAJcGotLvCIr4ytGaWoD6tmQJV1iMANlEiMsuvD+Wo0VOpPHMvN4tMpRn1JWCERZwHSjez1yJgq2cx2qsZTBfZ69XMbBO1P8meAvobaM0s5yxW9yk7hdwhL9r15ijCR5wBGho8ScgqBIvlTtdUF2V7BOses7yAjobs9VoDkxmkm2WlclxNgJiPOA20Vp8qisO4lqBp3tWmu0gSUeMEUDsuRnstfc6OXG762GV7vXgd6uXIJ9lTQHdllsUp7KXJlGcvs0wV1eeJILMGpG0qgsX85v2tdCtj2A9ZXkCPqj5X97AlE5tcSeQRp4BK7gg+AVHMIs9HaDdLl+ZQsU8A7Xqfu6pjMwaXIkoKERZxFmg70+WixGA1DFb7SFU+xjPFnOUZoNL7fJM9bEMSOx8R8xHngHbdcC8y+zSzNcLLxAxE1DgtNNUfPvIOOdH7PAP0/vEbVDNMAOlnbe2PamZoRhlTc/zyAjocKhaRBEwgdwXpZrQ/LjJYjHZcag0v66GNJ99TQPdA2SIGDwgV1vb+SvhZniQMKhmJ8u2T7ymge6DfnOmq1nclvXx7e/o9BRRQQAEFFFBAx0C/ADwlZbk/JKCmAAAAAElFTkSuQmCC"
 
 
 class TestSetup(unittest.TestCase):
@@ -16,10 +24,85 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer["portal"]
-        self.installer = get_installer(self.portal, self.layer["request"])
+        self.request = self.layer["request"]
 
     def test_sign_url(self):
         login = "user1"
         secret_key = "secret"
         signed_url = helpers.sign_url(login, secret_key, lifetime=10, url="")
         self.assertTrue(signed_url.startswith("?login=user1&"))
+
+
+# zope-testrunner -pvc --test-path=./src -t HelpersFunctionalTest
+class HelpersFunctionalTest(unittest.TestCase):
+    layer = PAS_PLUGINS_OTP_FUNCTIONAL_TESTING
+
+    def _manager_browser(self):
+        transaction.commit()
+        # Set up browser
+        browser = Browser(self.layer["app"])
+        browser.handleErrors = False
+        browser.addHeader(
+            "Authorization",
+            "Basic {}:{}".format(
+                SITE_OWNER_NAME,
+                SITE_OWNER_PASSWORD,
+            ),
+        )
+        return browser
+
+    def _anon_browser(self):
+        transaction.commit()
+        # Set up browser
+        browser = Browser(self.layer["app"])
+        browser.handleErrors = False
+        return browser
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+        setRoles(self.portal, TEST_USER_ID, ["Member"])
+        self.portal.acl_users.userFolderAddUser("user1", "secret", ["Member"], [])
+
+    def test_extract_ip_address_from_request(self):
+        from pas.plugins.tfa.helpers import extract_ip_address_from_request
+
+        # inject a IP
+        remote_ip = "10.10.10.11"
+        self.request.environ.update({"REMOTE_ADDR": remote_ip})
+        ip = extract_ip_address_from_request(self.request)
+        self.assertEqual(ip, remote_ip)
+
+        # inject a proxy IP
+        proxy_ips = "10.10.10.9, 10.10.10.10"
+        self.request.environ.update({"HTTP_X_FORWARDED_FOR": proxy_ips})
+        ip = extract_ip_address_from_request(self.request)
+        self.assertEqual(ip, "10.10.10.9")
+
+    def test_get_domain_name(self):
+        from pas.plugins.tfa.helpers import get_domain_name
+
+        domain = get_domain_name(self.request)
+        self.assertEqual(domain, "nohost")
+
+    def test_qr_code_generation(self):
+        from pas.plugins.tfa.helpers import get_barcode_image
+
+        # that's the plone user
+        testuser = self.portal.acl_users.getUser("user1")
+
+        # that's the memberdata object
+        # member = self.portal.portal_membership.getMemberById(testuser.getId())
+
+        # use a dummy domain
+        domain = "dummy-host.local"
+
+        # use a dummy secret
+        secret = "12345"
+
+        base64_image = get_barcode_image(
+            testuser.getId(),
+            domain,
+            secret,
+        )
+        self.assertEqual(str(base64_image), QR_CODE_REF)
