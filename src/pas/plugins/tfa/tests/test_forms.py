30dbe1315c7351ed7354d206e749c9f3a509dfd4
1letter <1letter@gmx.de>
2024-05-09 23:17:12 +0200

    add tests

--- src/pas/plugins/tfa/tests/test_forms.py
+++ src/pas/plugins/tfa/tests/test_forms.py
@@ -78,3 +78,54 @@ class TestFunctionalForms(FunctionalBase):
         browser.getControl(name="form.buttons.verify").click()
 
         self.assertIn("Welcome! You are now logged in", browser.contents)
+
+    def test_tfaddform_login_process_with_expired_token(self):
+        import pyotp
+        import time
+
+        member = self._getMember(TEST_USER_NAME)
+
+        secret = member.getProperty("two_factor_authentication_secret")
+
+        # the secret is empty
+        self.assertFalse(secret, "The secret should be empty")
+
+        browser = self._anon_browser()
+        browser.open(f"{self.portal.absolute_url()}/login")
+        browser.getControl(name="__ac_name").value = TEST_USER_NAME
+        browser.getControl(name="__ac_password").value = TEST_USER_PASSWORD
+        browser.getControl(name="buttons.login").click()
+
+        member = self._getMember(TEST_USER_NAME)
+        secret = member.getProperty("two_factor_authentication_secret")
+
+        # the secret is not empty
+        self.assertTrue(secret, "The secret should not be empty")
+
+        # use the secret, we didn't scan the qr-code in tests
+        totp = pyotp.TOTP(secret)
+
+        # first we wait a little bit, until the token is invalid and then we click verify
+        token = totp.now()
+        browser.getControl(name="form.widgets.token").value = token
+
+        # wait with the click
+        time.sleep(40)
+        browser.getControl(name="form.buttons.verify").click()
+
+        self.assertIn("Invalid token or token expired", browser.contents)
+
+    def test_tfaddform_login_process_with_invalid_token(self):
+
+        browser = self._anon_browser()
+        browser.open(f"{self.portal.absolute_url()}/login")
+        browser.getControl(name="__ac_name").value = TEST_USER_NAME
+        browser.getControl(name="__ac_password").value = TEST_USER_PASSWORD
+        browser.getControl(name="buttons.login").click()
+
+        # we guess the token
+        token = "00195300"
+        browser.getControl(name="form.widgets.token").value = token
+        browser.getControl(name="form.buttons.verify").click()
+
+        self.assertIn("Invalid token or token expired", browser.contents)
