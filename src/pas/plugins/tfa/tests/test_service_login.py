b884ab26d88d38a9670cbe41cef7c9a99b3b61d0
1letter <1letter@gmx.de>
2024-05-11 00:27:22 +0200

    add test for login service endpoint

---
+++ src/pas/plugins/tfa/tests/test_service_login.py
@@ -0,0 +1,56 @@
+from pas.plugins.tfa.tests.base import FunctionalBase
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
+
+import transaction
+
+
+class ServiceEndpointLoginFunctionalTest(FunctionalBase):
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Member"])
+
+    def test_login_without_2FA(self):
+        import requests
+
+        # login without "2FA"
+        response = requests.post(
+            f"{self.portal.absolute_url()}/@login",
+            headers={"Accept": "application/json"},
+            json={"login": TEST_USER_NAME, "password": TEST_USER_PASSWORD},
+        )
+        transaction.commit()
+
+        self.assertIn("token", response.text)
+
+    def test_login_with_2FA(self):
+        import json
+        import requests
+
+        member = self._getMember(TEST_USER_NAME)
+
+        # enable 2fa
+        member.setMemberProperties(mapping={"two_factor_authentication_enabled": True})
+
+        transaction.commit()
+
+        # login
+        response = requests.post(
+            f"{self.portal.absolute_url()}/@login",
+            headers={"Accept": "application/json"},
+            json={"login": TEST_USER_NAME, "password": TEST_USER_PASSWORD},
+        )
+        transaction.commit()
+
+        data = json.loads(response.text)
+        self.assertIn("action", data.keys())
+        self.assertIn("login", data.keys())
+        self.assertIn("qr_code", data.keys())
+        self.assertIn("type", data.keys())
+        self.assertIn("add", data.get("action"))
+        self.assertIn(TEST_USER_ID, data.get("login"))
+        self.assertIn("otpauth://", data.get("qr_code"))
+        self.assertIn("totp", data.get("type"))
